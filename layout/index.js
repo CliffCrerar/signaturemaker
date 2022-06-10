71344bed1b5f696e9d99486bee658f460a122917
Clifford Crerar <cliff.crerar@gmail.com>
2022-06-11 01:08:52 +0200

    minor

--- layout/index.js
+++ layout/index.js
@@ -1,4 +1,6 @@
+import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import { useRef, useEffect } from "react";
 
 
@@ -15,6 +17,13 @@ function Layout(props) {
 
     return (
         <>
+            <Head>
+                <meta charset="utf-8" />
+                <meta name="Description" CONTENT="Author: C. Crerar, Email Signature Generator" />
+                <meta name="google-site-verification" content="+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34=" />
+                <title>Email Signature Generator</title>
+                <meta name="robots" content="noindex,nofollow" />
+            </Head>
             <style jsx global>
                 {`
             #__next, .__next {
@@ -56,36 +65,49 @@ function Layout(props) {
             `}
             </style>
             <div className="__next">
-            <header ref={navRef}>
-                <nav>
-                    <div>
+                <header ref={navRef}>
+                    <nav>
                         <div>
-                            <h1>Generate Signature</h1>
+                            <div>
+                                <h1>Generate Signature</h1>
+                            </div>
+                                {/* <ul style={{display: 'block', alignContent: 'center', flex: '1', alignSelf: "flex-end"}}>
+                                    <li>
+                                        <Link href="/">
+                                            <a title="home">Home</a>
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/instructions">
+                                            <a title="home">Home</a>
+                                        </Link>
+                                    </li>
+                                </ul> */}
+                                <a href="https://infinityarc.net" target="_blank">
+                                <Image src={'https://cdn.softwarecraft.co.za/assets/ia/ia-3d-black05.png'} width={'20px'} height={'20px'} />
+                                </a>
+                            
                         </div>
-                        <div>
-                            <Image src={'/favicon.ico'} width={'20px'} height={'20px'} />
-                        </div>
-                    </div>
-                </nav>
-            </header>
-            <main>
-                {props.children}
-            </main>
+                    </nav>
+                </header>
+                <main>
+                    {props.children}
+                </main>
             </div>
             <footer>
                 <div>
-                <img src="https://cdn.softwarecraft.co.za/assets/ia/ia-full-logo-v3-outl-025.png" height={'100px'}></img>
+                    <img src="https://cdn.softwarecraft.co.za/assets/ia/ia-full-logo-v3-outl-025.png" height={'100px'}></img>
                 </div>
                 <div>
-                <a style={{textDecoration: 'none'}} href="mailto:support@infinityarc.net">Get Support</a>
+                    <a style={{ textDecoration: 'none' }} href="mailto:support@infinityarc.net">Get Support</a>
                 </div>
                 <div>
                     <div>Developer: Infinity Arc</div>
                     <div>
-                       Website: <a href="https://infinityarc.net" target="_blank">www.infinityarc.net</a></div>
+                        Website: <a href="https://infinityarc.net" target="_blank">www.infinityarc.net</a></div>
                 </div>
             </footer>
-            
+
         </>
     )
 }
---
+++ pages/instructions.js
@@ -0,0 +1,11 @@
+import Layout from "../layout"
+
+export default () => {
+    return (
+        <Layout>
+        <div className="container">
+            Instructions Under Dev
+        </div>
+        </Layout>
+    )
+}
